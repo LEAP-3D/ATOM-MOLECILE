db16a3b1737c2795773f379e304a0a5fcd5adf58
25LP3051 25LP3051 <25LP3051@G302-CC0097.local>
2026-01-26 11:52:24 +0800

    cd ci yaml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 const Home = () => {
-  return <>hello from home Bayraa shit like that</>;
+  return <>hello from home Bayraa shit like that cd ci yaml change</>;
 };
 
 export default Home;
