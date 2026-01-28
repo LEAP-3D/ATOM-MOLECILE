16d3aaca17a925788f9dbea5e672ad392873d8ee
25LP3051 25LP3051 <25LP3051@G302-CC0097.local>
2026-01-28 15:44:19 +0800

    ci setup preview vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,6 +105,7 @@ export default function Home() {
         <div className="flex justify-center items-center gap-4">
           <span>Undak</span>
           <span>Beeb</span>
+          <span>Bayaraa</span>
           <span>Tumee</span>
         </div>
       </Marquee>
