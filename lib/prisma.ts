8fc6dacbb7b9bd189043822a1fd42337c6c99f3e
25LP3051 25LP3051 <25LP3051@G302-CC0097.local>
2026-01-28 14:18:01 +0800

    prisma fixed 2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,38 @@
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient;
+// };
+
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     adapter,
+//   });
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const globalForPrisma = global as typeof globalThis & {
   prisma: PrismaClient;
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const prisma =
+export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
+    log: ["query"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
