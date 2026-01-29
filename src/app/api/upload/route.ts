0799e6b6cdc195385e78a877e86faeda0f4ca31c
chinkhuu <42198023+chinkhuu@users.noreply.github.com>
2026-01-29 13:30:07 +0800

    add prisma upload file

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -10,7 +10,7 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 // }
 
 const supabase = createClient(supabaseUrl, supabaseKey);
-
+console.log('Supabase client initialized', supabaseUrl);
 export async function POST(request: Request) {
     if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
     return NextResponse.json({ error: 'Серверийн тохиргоо дутуу байна.' }, { status: 500 });
